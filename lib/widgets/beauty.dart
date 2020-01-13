26d90b4c4f113dbebbe2bd3683e80ce0671a6570
winkyawtkhaing <winkyawtkhaing2017@gmail.com>
2020-01-13 11:31:28 +0630

    beautydetails

--- lib/widgets/beauty.dart
+++ lib/widgets/beauty.dart
@@ -4,6 +4,7 @@ import 'package:networking_demo/models/newsArticle.dart';
 import 'package:networking_demo/services/webservice.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:networking_demo/utils/constants.dart';
+import 'package:networking_demo/widgets/detail_page.dart';
 
 class BeaudyListState extends State<BeaudyList> {
   List<NewsArticle> _beautyArticles = List<NewsArticle>();
@@ -41,32 +42,47 @@ class BeaudyListState extends State<BeaudyList> {
         gridDelegate:
             new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext content, int index) {
-          return Card(
-            color: Color(0xffDDECEF),
-            elevation: 5,
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  flex: 2,
-                  child: _beautyArticles[index].urlToImage == null
-                      ? Padding(
-                          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                          child: Image.asset(
-                            Constants.NEWS_PLACEHOLDER_IMAGE_ASSET_URL,
+          return new GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          DetailPage(newsarticle: _beautyArticles[index])));
+            },
+            child: Card(
+              color: Color(0xffDDECEF),
+              elevation: 5,
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: _beautyArticles[index].urlToImage == null
+                        ? Padding(
+                            padding:
+                                const EdgeInsets.only(left: 8.0, right: 8.0),
+                            child: Image.asset(
+                              Constants.NEWS_PLACEHOLDER_IMAGE_ASSET_URL,
+                            ),
+                          )
+                        : Padding(
+                            padding:
+                                const EdgeInsets.only(left: 8.0, right: 8.0),
+                            child: Image.network(
+                              _beautyArticles[index].urlToImage,
+                            ),
                           ),
-                        )
-                      : Padding(
-                          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                          child: Image.network(
-                            _beautyArticles[index].urlToImage,
-                          ),
-                        ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(_beautyArticles[index].title, style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
-                )
-              ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      _beautyArticles[index].title,
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
             ),
           );
         },
@@ -85,43 +101,3 @@ class BeaudyList extends StatefulWidget {
   @override
   createState() => BeaudyListState();
 }
-
-/*return new Container(
-            child: new Card(
-              elevation: 5,
-              child: new Column(
-                children: <Widget>[
-                  new Hero(
-                    tag: _beautyArticles[index].title,
-                    child: new Material(
-                      child: new InkWell(
-                        // onTap: ()=> Navigator.of(context).push(new MaterialPageRoute(
-                        //     builder: (BuildContext context)=> new Detail(name: jdata["name"], image: jdata["image"],)
-                        // )),
-                        child: _beautyArticles[index].urlToImage == null
-                            ? Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 8.0, right: 8.0),
-                                child: Image.asset(
-                                  Constants.NEWS_PLACEHOLDER_IMAGE_ASSET_URL,
-                                ),
-                              )
-                            : Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 8.0, right: 8.0),
-                                child: Image.network(
-                                  _beautyArticles[index].urlToImage,
-                                ),
-                              ),
-                      ),
-                    ),
-                  ),
-                  new Padding(padding: EdgeInsets.all(10.0)),
-                  new Text(
-                    _beautyArticles[index].title,
-                    style: new TextStyle(fontSize: 18.0),
-                  )
-                ],
-              ),
-            ),
-          ); */
